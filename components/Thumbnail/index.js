9989663517c3251d16ff3fd52f550141d70a7530
shamaMckc <shama_ahlawat@external.mckinsey.com>
2021-02-20 01:26:42 +0530

    lonly needed content loaded not refresh and fetch everything

--- components/Thumbnail/index.js
+++ components/Thumbnail/index.js
@@ -3,7 +3,7 @@ import Link from 'next/link';
 const Thumbnail = ({imageUrl='https://via.placeholder.com/150',caption}) => {
     return (
         <div>
-            <Link href="/us/5617" as="/us/5617">
+            <Link href="/[country]/[showId]" as="/us/5617">
             <a>
             <img className="image" src={imageUrl} />
             <div className="text">{caption}</div>
