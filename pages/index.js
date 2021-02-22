7454713cd157d7cf0d3c6ad79b95e24222cf6413
shamaMckc <shama_ahlawat@external.mckinsey.com>
2021-02-22 20:12:28 +0530

    how to set cookies during signIn

--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,6 @@
 // import Head from 'next/head'
 // import styles from '../styles/Home.module.css'
+import axios from 'axios';
 import Router from 'next/router';
 
 export default function Home() {
@@ -21,3 +22,29 @@ Home.getInitialProps = (context) => {
   }
 }
 // we have two ways to redirect user either from client side or server side. both are different ... with browser property we get to know
+
+
+
+//to set routing after login
+
+// const authenticate = context => {
+//   const {token} = cookies.get(context); //checking if cookies present , if it is not present ,redirect user to signin page
+//   cookies.set(context,'plannedRoute', JSON.stringify({as:context.asPath,href:context.pathname}),{path:'/'})
+//   if(!token){
+//     Router.push('/signin')
+//   }
+//   try{
+//     const res = await axios.post('path', {..signinfo});
+//     cookies.set(null,'token',response.data.token,{path:'/'})
+//     const {plannedRoute} = cookies.get()
+//     const parsedPlannedRoute = plannedRoute && JSON.parse(plannedRoute)
+//     const plannedHrefRoute = parsedPlannedRoute ? parsedPlannedRoute.href : '/[country]'
+//     const plannedAsRoute = parsedPlannedRoute ? parsedPlannedRoute.as : 'us'
+//     // router.replace('/[country]','/us')
+//     router.replace(plannedHrefRoute,plannedAsRoute)
+//   }
+// }
+
+// const handleSignout = () => {
+//   cookies.destroy(null,'token')
+// }
